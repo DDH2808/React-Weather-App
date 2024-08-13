b009c0f7b792d94531ad23ccaf4b871a49c9963a
DDH2808 <daodinhhoan2002@gmail.com>
2024-08-13 17:48:46 +0700

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '4debd56969msh37fa19e07a85507p18f1cejsn97d5ccc8e976',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "236057abb35f5826cfd7eafa058dc28b"; // enter your key from openweather API
